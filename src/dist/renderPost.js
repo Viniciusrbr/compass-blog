8239e998dfe067e6c21e1bb58d2c65380de98730
Vinicius Ribeiro <viniciusgoes159@gmail.com>
2023-07-30 22:00:53 -0300

    pagina inicial: adicionando hover aos cards

--- src/dist/renderPost.js
+++ src/dist/renderPost.js
@@ -6,9 +6,11 @@ let allPosts = posts.reduce((allPosts, post) => {
 <div class="post-card">
   <img src="${post.imageUrl}" alt="post image">
   <div class="post-texts">
+    <a href="postdetails.html?id=${post.id}"
     <h2 class="post-title">${post.title}</h2>
     <p class="post-content">${post.body}</p>
-    <a href="postdetails.html?id=${post.id}" class="post-details">Expand...</a>
+    <p class="post-details">Expand...</p>
+    </a>
   </div>
 </div>
         `);
